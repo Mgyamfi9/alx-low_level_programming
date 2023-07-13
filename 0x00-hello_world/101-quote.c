c566ab6e271a983261177b31ac40ccc96002179c
Mgyamfi9 <mhikegyamphi@gmail.com>
2023-07-13 05:46:52 -0700

    corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,18 +13,18 @@
 int main(void)
 {
 	int stderr_fd;
-    	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    	size_t message_len = strlen(message);
-    	stderr_fd = open("/dev/stderr", O_WRONLY);
-    	if (stderr_fd == -1) 
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t message_len = strlen(message);
+	stderr_fd = open("/dev/stderr", O_WRONLY);
+	if (stderr_fd == -1) 
 	{
-        	return (1);
-    	}
-    	if (write(stderr_fd, message, message_len) != (ssize_t)message_len) 
+		return (1);
+	}
+	if (write(stderr_fd, message, message_len) != (ssize_t)message_len) 
 	{
-        	close(stderr_fd);
-        	return (1);
-    	}
-    	close(stderr_fd);
-    	return (1);
+		close(stderr_fd);
+		return (1);
+	}
+	close(stderr_fd);
+	return (1);
 }
