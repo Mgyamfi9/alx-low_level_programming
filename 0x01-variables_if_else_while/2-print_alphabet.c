2d622d202f12c64495695715bff93b04b2aeee74
Mike Gyamfi <mhikegyamphi@gmail.com>
2023-07-14 13:32:43 +0000

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,9 +12,9 @@
 int main(void)
 {
         int ch;
-        for (ch = 65; ch <= 90; ch++)
+        for (ch = 97; ch <= 122; ch++)
         {
-          putchar(ch);
+                putchar(ch);
         }
         return (0);
 }
