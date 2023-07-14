add9e967cb60abfda36032bd659e4369450b027b
Mike Gyamfi <mhikegyamphi@gmail.com>
2023-07-14 14:07:58 +0000

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+  int alpha=65
+	/*declaring ch*/
+	while (ch <= 'z' && alpha <= 90)
+	{
+		putchar(ch);
+    putchar(alpha)
+		ch++;
+	}
+	/*adding new line*/
+	putchar('$\n');
+	/*returning 0*/
+	return (0);
+}
