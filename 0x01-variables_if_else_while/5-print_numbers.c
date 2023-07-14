ea01428d43a81ac82594c0ff8fc9c72dd10aa268
Mgyamfi9 <mhikegyamphi@gmail.com>
2023-07-14 08:24:35 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int i;
+	/*declaring variables*/
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d\n", i);
+	}
+	/*returning values*/
+	return (0);
+}
