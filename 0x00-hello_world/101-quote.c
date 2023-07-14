aebcedc102117d9d4a4da4bd102d4033d360e891
Mike Gyamfi <mhikegyamphi@gmail.com>
2023-07-14 06:07:11 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,30 +1,14 @@
+#include <stdio.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 
 /**
- * main - Entry point of the program
+ * main - Entry point
  *
- * Return: 1 (Error)
+ * Return: 1
  */
+
 int main(void)
 {
-	int stderr_fd;
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	size_t message_len = strlen(message);
-	stderr_fd = open("/dev/stderr", O_WRONLY);
-	if (stderr_fd == -1) 
-	{
-		return (1);
-	}
-	if (write(stderr_fd, message, message_len) != (ssize_t)message_len) 
-	{
-		close(stderr_fd);
-		return (1);
-	}
-	close(stderr_fd);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
