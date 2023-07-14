249c9b3541452103f88fec0e619b08a2a3e28965
Mike Gyamfi <mhikegyamphi@gmail.com>
2023-07-14 13:26:01 +0000

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int ch;
+        for (ch = 65; ch <= 90; ch++)
+        {
+          putchar(ch);
+        }
+        return (0);
+}
