a07146fa8463827357dada25251f39a7708b5249
Mgyamfi9 <mhikegyamphi@gmail.com>
2023-07-14 09:46:04 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	/*declaring ch*/
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch--;
+	}
+	/*adding new line*/
+	putchar('\n');
+	/*returning 0*/
+	return (0);
+}
