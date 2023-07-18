5fe2043ffee6846e22797e1b9b7886ff584a0fa3
Mike Gyamfi <mhikegyamphi@gmail.com>
2023-07-18 19:32:42 +0000

    Create 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_times_table - prints timetable
+* @n: value to use in code
+*/
+void print_times_table(int n)
+{
+	if (n < 0 || n > 15)
+	{
+		return (0);
+	}
+    int i, j;
+    /*variable declarations*/
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			int result = i * j;
+			if (j != 0)
+			{
+				_putchar(",   ");
+				if (result < 10)
+				{
+					_putchar(" ");
+				}
+				if (result < 100)
+				{
+					_putchar(" ");
+				}
+            _putchar(result);
+			}
+			_putchar("\n");
+		}
+	}
+}
