2646d2ac3af2b886bf1e05372aaa22ed543f7d87
Mike Gyamfi <mhikegyamphi@gmail.com>
2023-07-18 18:53:44 +0000

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,6 +5,7 @@
 void times_table(void)
 {
 	int row, column, product;
+	/*declarations*/
 	for (row = 0; row <= 9; row++)
 	{
 		for (column = 0; column <= 9; column++)
@@ -27,5 +28,4 @@ void times_table(void)
 		}
 		_putchar('\n');
 	}
-	return (0);
 }
