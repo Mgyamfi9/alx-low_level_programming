754994e40fec592f4fd214476bdc8c3bcf5789c4
Mike Gyamfi <mhikegyamphi@gmail.com>
2023-07-18 20:05:10 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,14 @@
  */
 int main(void)
 {
-	putchar('_putchar');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	putchar('\n');
 	return (0);
 }
