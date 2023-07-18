101cd7c8f7a5e8e3650312462c143190214c5439
Mike Gyamfi <mhikegyamphi@gmail.com>
2023-07-18 18:58:20 +0000

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code
- *
+ * add - add arguments
+ *@x: first int value
+ *@y: second int value
  * Return: Always 0.
  */
-int add( int x, int y )
+int add(int x, int y)
 {
 	int result;
+	/*declarations*/
 	result = x + y;
 	return result;
 }
