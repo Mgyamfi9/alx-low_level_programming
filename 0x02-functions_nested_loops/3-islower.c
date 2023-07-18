1eb3d74307f8b400821dc3920d1e13b461daa165
Mike Gyamfi <mhikegyamphi@gmail.com>
2023-07-18 15:47:39 +0000

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -10,12 +10,12 @@ int _islower(int c)
 {
 	if (islower(c))
 	{
-		_putchar('1' + '0');
+		_putchar('1');
 		return (1);
 	}
 	else
 	{
-		_putchar('0' + '0');
+		_putchar('0');
 		return (0);
 	}
 }
