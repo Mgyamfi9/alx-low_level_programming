dea063198d08e91a7b1ad32be24a9dcae460f85f
Mike Gyamfi <mhikegyamphi@gmail.com>
2023-07-18 15:33:55 +0000

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-/**
-* print_alphabet - prints alphabets a-z
-*
+* print_alphabet - prints alphabets
 */
 void print_alphabet(void)
 {
