8a989cbcf3b9c432e29fa332143aeff1c7e7e9c1
Mike Gyamfi <mhikegyamphi@gmail.com>
2023-07-18 20:12:12 +0000

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,13 +11,14 @@ void print_times_table(int n)
 	/*variable declarations*/
 	if (n < 0 || n > 15)
 	{
-		 return;
+		return;
 	}
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
 			int result;
+			/*declarations*/
 			result = i * j;
 			if (j != 0)
 			{
@@ -30,7 +31,7 @@ void print_times_table(int n)
 				if (result < 100)
 				{
 					_putchar(' ');
-					_putchar(result % 10 +'0');
+					_putchar(result + '0');
 				}
 				_putchar(result % 10 + '0');
 			}
