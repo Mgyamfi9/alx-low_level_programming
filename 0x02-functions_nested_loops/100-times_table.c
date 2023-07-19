4cc77643cba48ad73d1114d130673044d0a4f7df
Mike Gyamfi <mhikegyamphi@gmail.com>
2023-07-19 01:01:30 +0000

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,39 +8,45 @@ void print_times_table(int n)
 {
 	int i;
 	int j;
+	int k;
 	/*variable declarations*/
-	if (n < 0 || n > 15)
+	if (n >= 0 && n <= 15)
 	{
-		return;
-	}
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			int result;
-			/*declarations*/
-			result = i * j;
-			if (j != 0)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
-				if (result < 10)
+				k = j * i;
+				if ( j == 0)
 				{
-					_putchar(' ');
-					_putchar(' ');
+					_putchar(k + '0');
 				}
-				else if (result < 100)
+				else if (k < 10 && j !=0)
 				{
-					_putchar(' ');
-					_putchar(' ');
+					_putchar(',');
+					_putchar('');
+					_putchar('');
+					_putchar('');
+					_putchar(k + '0');
 				}
-				_putchar(result % 10 + '0');
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar('');
+					_putchar('');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar('');
+					_putchar((k / 100) + '0');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
 			}
-			else
-			{
-				_putchar('0');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
