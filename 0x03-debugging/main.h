ed20b5086777e47a755f0be83fa6bf94322d7751
Mike Gyamfi <mhikegyamphi@gmail.com>
2023-07-20 11:06:54 +0000

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-/**
- * positive_or_negative - checks if positive or negative
- * @i: value to check
-*/
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+#endif
