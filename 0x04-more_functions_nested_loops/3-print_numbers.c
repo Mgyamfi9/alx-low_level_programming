8916896f25cec949ade2b1515ccc3e6b269f87c3
Mgyamfi9 <mhikegyamphi@gmail.com>
2023-07-20 06:19:29 -0700

    bug fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 /**
  * print_numbers - prints numbers
- * Return: 0 success
+ * 
  */
 void print_numbers(void)
 {
 	int i;
 	/*variable declarations*/
-	i = 0;
-	for (i; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		_putchar('0' + i);
 	}
