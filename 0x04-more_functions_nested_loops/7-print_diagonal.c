f528c093b7e32a4820c0a2ff04a7e985a1b61584
Mgyamfi9 <mhikegyamphi@gmail.com>
2023-07-20 07:33:05 -0700

    bugs

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagonal - prints diagonal
+ *
+*/
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+		/*variable declaration*/
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
