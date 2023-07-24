1a85700989855f19d97349bd4cbbc5051a1d8ec6
Mgyamfi9 <mhikegyamphi@gmail.com>
2023-07-24 04:01:52 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - updates the value to 98 using the pointer
+ * @*n: pointer variable
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - swaps the integers
+ * @*a: first pointer value
+ * @*b: second pointer value
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
