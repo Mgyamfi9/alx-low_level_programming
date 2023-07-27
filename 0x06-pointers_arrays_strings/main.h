71a31f0eb318650fec910cd37ab875ee4e7e6421
Mike Gyamfi <mhikegyamphi@gmail.com>
2023-07-27 12:01:04 +0000

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,7 +7,7 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char);
-char *strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
