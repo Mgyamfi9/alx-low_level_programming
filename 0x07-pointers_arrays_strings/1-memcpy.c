e41678be671c5fc06b0a97309baaccb60bf2b85c
Mike Gyamfi <mhikegyamphi@gmail.com>
2023-07-31 13:00:32 +0000

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,7 +6,7 @@
  * @dest: destination to copy to
  * @src: destination to copy from
  * @n: bytes to copy
- * Return: memcpy(dest, src, n) pointer to dest
+ * Return: memcpy(dest, src, n)
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
