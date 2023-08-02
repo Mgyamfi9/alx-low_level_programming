49fa454f420ba2efbcb56702d2f0a67174d522fe
Mgyamfi9 <mhikegyamphi@gmail.com>
2023-08-02 07:17:57 -0700

    strlen

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,20 +7,5 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int l, c;
-	char *b, *e, t;
-	/*variables*/
-	l = strlen(s);
-	b = s;
-	e = s;
-	for (c = 0; c < l - 1; c++)
-		e++;
-	for (c = 0; c < l / 2; c++)
-	{
-		t = *e;
-		*e = *b;
-		*b = t;
-		b++;
-		e--;
-	}
+	strrev(s);
 }
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strlen_recursion: length of string
+ * @s: string 
+ * Return: strlen(s) length of string
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
