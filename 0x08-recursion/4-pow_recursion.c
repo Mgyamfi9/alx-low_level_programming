f163445cde618333ef07b6e2191e11dce70ed3fa
Mike Gyamfi <mhikegyamphi@gmail.com>
2023-08-02 21:07:17 +0000

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,16 +11,9 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 	       return (-1);
-	if ( y == 0)
-	{
-		return (1);
-	}
-	else if ( y == 1)
+	if ( y == 1)
 	{
 		return (x);
 	}
-	else
-	{
-		return (pow(x, y));
-	}	
+	return (x * _pow_recursion(x, y));
 }
