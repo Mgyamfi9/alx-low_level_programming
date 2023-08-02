b017f966c86cef98381536d533ed7e6799f8b18b
Mgyamfi9 <mhikegyamphi@gmail.com>
2023-08-02 06:57:24 -0700

    reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,17 +5,5 @@
  */
 void _print_rev_recursion(char *s)
 {
-	char rev = s[0];
-	int counter = 0;
-	int i;
-	/*variables*/
-	while (s[counter] != '\0')
-		counter++;
-	for (i = 0; i < counter; i++)
-	{
-		counter--;
-		rev = s[i];
-		s[i] = s[counter];
-		s[counter] = rev;
-	}
+	strrev(s);
 }
