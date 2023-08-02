4070a85858641f9015df12358e85caaf9b6890d6
Mgyamfi9 <mhikegyamphi@gmail.com>
2023-08-02 06:53:03 -0700

    reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to print
+ */
+void _print_rev_recursion(char *s)
+{
+	char rev = s[0];
+	int counter = 0;
+	int i;
+	/*variables*/
+	while (s[counter] != '\0')
+		counter++;
+	for (i = 0; i < counter; i++)
+	{
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev
+	}
+}
