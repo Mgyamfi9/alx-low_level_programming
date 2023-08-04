224e5cb31f55364ec5a1dd6030993a04ebb97968
Mike Gyamfi <mhikegyamphi@gmail.com>
2023-08-04 01:43:13 +0000

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,7 +6,7 @@
  * @argv: pointer array
  * Return: 0 always success
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
         while(argc--)
                 printf("%s\n", *argv++);
