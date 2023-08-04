0bf7589896354133d05f3ff817a8c77bd49ad897
Mike Gyamfi <mhikegyamphi@gmail.com>
2023-08-04 02:32:45 +0000

    Create 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*miinimum_coins - minimum coins to find
+*@cents - amount of cents to give back
+*Return: 0 always success
+*/
+int minimum_coins(int cents)
+{
+	if (cents < 0)
+	{
+		return (0);
+	}
+	int coins[] = {25, 10, 5, 2, 1};
+	int num_coins = 0;
+	for (int i = 0; i < sizeof(coins) / sizeof(coins[0]); i++)
+	{
+		num_coins += cents / coins[i];
+		cents %= coins[i];
+	}
+	return (num_coins);
+}
+/**
+*main - entry point of program
+*@argc: size of argv
+*@argv:pointer array of arguments
+*Return: 0 always success
+*/
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	int cents = atoi(argv[1]);
+	printf("%d\n", minimum_coins(cents));
+	return (0);
+}
