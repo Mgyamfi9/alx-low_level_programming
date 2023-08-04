2409904efaf6506de6a2368ca711123456ee4be1
Mike Gyamfi <mhikegyamphi@gmail.com>
2023-08-04 02:00:17 +0000

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - entry point of program
  *@argc: size of argv
@@ -7,6 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
+(void argv);
 	printf("%d\n", argc - 1);
 	return (0);
 }
