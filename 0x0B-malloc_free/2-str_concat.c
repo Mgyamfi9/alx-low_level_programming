0d4804f003c960bfc655f2e6ff759c0bfa530ad8
Mgyamfi9 <mhikegyamphi@gmail.com>
2023-08-08 04:23:11 -0700

    strcat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,11 +11,35 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
-	char *str
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int len1 = 0;
+	unsigned int len2 = 0;
 	/*variable declaration*/
-	ptr = strcat(s1, s2); 
-	*str = malloc(ptr * sizeof(char));
-	return (str);
-	if (str == NULL)
-		return (NULL); 
+      	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (ptr == NULL)
+		return (NULL);
+	if (s1)
+	{
+		while (i < len1)
+		{
+			ptr[i] = s1[i];
+			i++;
+		}
+	}
+	if (s2)
+	{
+		while (i < (len1 + len2))
+		{
+			ptr[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+	ptr[i] = '\0';
+	return (ptr); 
 }
