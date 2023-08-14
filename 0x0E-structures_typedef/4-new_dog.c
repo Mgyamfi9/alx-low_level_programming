ca9fb68fce4bf08c24ce0cd49a189fa51b13f664
Mgyamfi9 <mhikegyamphi@gmail.com>
2023-08-14 06:42:18 -0700

    betty

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,8 +13,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new = (dog_t *)malloc(sizeof(dog_t));
 	if (new == NULL)
 		return (NULL);
-	new -> name = name;
-	new -> age = age;
-	new -> owner = owner;
+	new->name = name;
+	new->age = age;
+	new->owner = owner;
 	return (new);
 }
