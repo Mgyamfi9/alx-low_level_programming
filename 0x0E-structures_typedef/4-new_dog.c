ebb231d00fc2f9b36133017eac1e987cb9dc2549
Mgyamfi9 <mhikegyamphi@gmail.com>
2023-08-14 07:02:45 -0700

    betty

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,7 +6,7 @@
  * new_dog - new function
  * @name: name of dog
  * @age: age of dog
- * owner: owner of dog
+ * @owner: owner of dog
  * Return: new struct
  */
 dog_t *new_dog(char *name, float age, char *owner)
