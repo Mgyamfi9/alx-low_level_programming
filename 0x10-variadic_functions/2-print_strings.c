cbb05d201fef79c49bfa92c7ffe5a6289548398a
Mgyamfi9 <mhikegyamphi@gmail.com>
2023-08-17 17:23:14 -0700

    try

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,9 +14,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	/*variables*/
 	nstring = "(nil)";
 	va_start(strings, n);
-	str = va_arg(strings, char *);
 	for (i = 0; i < n; i++)
 	{
+		str = va_arg(strings, char *);
 		if (str == NULL)
 			printf("%s", nstring);
 		else
