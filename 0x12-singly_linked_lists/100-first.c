4b1dcf834fa27c894afde41bec5a4e7099203c1b
Mgyamfi9 <mhikegyamphi@gmail.com>
2023-08-24 18:23:25 -0700

    fixed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,5 +5,5 @@
 void __attribute__((constructor)) func(void)
 {
 	printf("You're beat! and yet, you must allow,");
-	printf("\nI bore my house upon my back!");
+	printf("\nI bore my house upon my back!\n");
 }
