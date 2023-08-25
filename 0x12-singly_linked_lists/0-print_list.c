89c6c5c92937e05ce958bf1df8835b8eb2d8eb02
Mgyamfi9 <mhikegyamphi@gmail.com>
2023-08-24 18:46:23 -0700

    fixed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,9 +9,9 @@ size_t print_list(const list_t *h)
 	size_t number_el;
 	/*variable declared*/
 	number_el = 0;
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
+		if (!h)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
