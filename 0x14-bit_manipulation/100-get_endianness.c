6392f670b47a293ed5f41feeec4677883850590d
Mgyamfi9 <mhikegyamphi@gmail.com>
2023-08-31 07:21:59 -0700

    bitwise operations

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * get_indianness - checks the endianness
+ * get_endianness - checks the endianness
  * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,9 +7,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	int b;
+
+	b = (n >> index);
 	if (index >= sizeof(n) * 8)
 		return (-1);
-	unsigned long int i_val = 1UL << index;
-
-	return ((n & i_val) != 0);
+	return (b & 1);
 }
