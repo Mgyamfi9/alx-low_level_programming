5d2b04bdfc49dbdb9ebe47222900c363fe33413b
Mgyamfi9 <mhikegyamphi@gmail.com>
2023-09-04 13:27:23 -0700

    errors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	fp = open(filename, O_WRONLY | O_APPEND);
-	if (fd < 0)
+	if (fp < 0)
 		return (-1);
 	if (text_content)
 	{
